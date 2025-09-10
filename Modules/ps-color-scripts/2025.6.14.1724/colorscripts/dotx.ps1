d64366acfb45b36b34e9250aa969f1c6bcda12f1
Tsangyao Chen <tychen742@gmail.com>
2025-09-09 20:02:56 -0500

    Please enter the coss
    mmit message for your changes. Lines starting
    this is not right? deletdedddsssss lslslsllslslsle

--- Modules/ps-color-scripts/2025.6.14.1724/colorscripts/dotx.ps1
+++
@@ -1,8 +0,0 @@
-﻿$esc = [char]27
-
-Write-Host @"
-
-$esc[01;30m ▚ $esc[0;30m ▞ $esc[01;31m ▚ $esc[0;31m ▞ $esc[01;32m ▚ $esc[0;32m ▞ $esc[01;33m ▚ $esc[0;33m ▞ $esc[01;34m ▚ $esc[0;34m ▞ $esc[01;35m ▚ $esc[0;35m ▞ $esc[01;36m ▚ $esc[0;36m ▞ $esc[01;37m ▚ $esc[0;37m ▞
-$esc[01;30m ▞ $esc[0;30m ▚ $esc[01;31m ▞$esc[0;31m  ▚ $esc[01;32m ▞ $esc[0;32m ▚ $esc[01;33m ▞ $esc[0;33m ▚ $esc[01;34m ▞ $esc[0;34m ▚ $esc[01;35m ▞ $esc[0;35m ▚ $esc[01;36m ▞ $esc[0;36m ▚ $esc[01;37m ▞ $esc[0;37m ▚
-
-"@
